f45161d5f16925c055ffbe41c8e726ffb3ee075c
ntwari-egide <ntwariegide2@gmail.com>
2020-11-21 08:44:45 +0200

    fix the bug of adding new client

--- src/store/store.js
+++ src/store/store.js
@@ -1,6 +1,5 @@
 import {createStore} from "redux";
 import reducer from "../reducers/reducer";
-
 // we don't pass the reducer as the function parameter ,
 // but we pass the reducer as the function reference
 const store = createStore(reducer);
